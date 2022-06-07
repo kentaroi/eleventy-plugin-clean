75931d113f5c9675afef2a7e7cad3de4caecb465
Kentaro Imai <kentaroi@gmail.com>
2022-06-08 06:40:57 +0900

    Initial implementation

---
+++ test/fixtures/eleventy-project-directories-configured/.eleventy.js
@@ -0,0 +1,22 @@
+const clean = require("../../..");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(clean);
+
+  eleventyConfig.addPassthroughCopy("**/*.foo");
+  eleventyConfig.addPassthroughCopy({ "**/*.bar": "baz" });
+  eleventyConfig.addPassthroughCopy("src/**/*.qux");
+  eleventyConfig.addPassthroughCopy({ "src/**/*.quux": "quuz" });
+
+  eleventyConfig.addPassthroughCopy("corge/corge");
+  eleventyConfig.addPassthroughCopy({ "grault/grault": "garply" });
+  eleventyConfig.addPassthroughCopy("waldo/waldo");
+  eleventyConfig.addPassthroughCopy({ "fred/fred": "plugh" });
+
+  eleventyConfig.addPassthroughCopy("src/xyzzy/xyzzy");
+  eleventyConfig.addPassthroughCopy({ "src/thud/thud": "hoge" });
+  eleventyConfig.addPassthroughCopy("src/fuga/fuga");
+  eleventyConfig.addPassthroughCopy({ "src/piyo/piyo": "hogera" });
+
+  return { dir: { input: "src", output: "dist" } };
+};
---
+++ test/fixtures/eleventy-project/.eleventy.js
@@ -0,0 +1,12 @@
+const clean = require("../../..");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(clean);
+
+  eleventyConfig.addPassthroughCopy("**/*.foo");
+  eleventyConfig.addPassthroughCopy({ "**/*.bar": "baz" });
+  eleventyConfig.addPassthroughCopy("qux/qux");
+  eleventyConfig.addPassthroughCopy({ "quux/quux": "quuz" });
+  eleventyConfig.addPassthroughCopy("corge/corge");
+  eleventyConfig.addPassthroughCopy({ "grault/grault": "garply" });
+};
